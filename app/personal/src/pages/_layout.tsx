3b87eb90080823e859470443e6801fa91ef72deb
The Fall of Man <developer@iswork.in>
2025-02-11 18:51:01 -0800

    chore: make layout more react-y

--- app/personal/src/pages/_layout.tsx
+++ app/personal/src/pages/_layout.tsx
@@ -2,19 +2,18 @@ import { StyledApp, JSXApp, type JSXChild } from '@nosh/compact'
 import React from 'react'
 // @ts-expect-error - compilation happens at runtime
 import css from '../styles/globals.css'
-const Layout = (args: Record<string, string|React.JSX.Element>) => {
-  const children:React.JSX.Element = args.children as React.JSX.Element
+const NavList = (linkmaps: Record<string, string>) => (<ul>{Object.keys(linkmaps).map((key) => (<a href={linkmaps[key]}><li>{key}</li></a>))}</ul>)
+const NavBar = ({ items }: {items: (JSXChild|string)[]}) => (<ul id="navbar">{items.map((item) => (<li>{item}</li>))}</ul>)
+const Layout = (args: Record<string, string | React.JSX.Element>) => {
+  const children: React.JSX.Element = args.children as React.JSX.Element
   return (
     <StyledApp styles={[css]}>
       <h1 id="layout-headline">Indomitable Pragma</h1>
-      <ul id="navbar">
-        <li><a href="/">Home</a></li>
-        <li>Stuff<ul>
-          <a href="/page1"><li>Page 1</li></a>
-          <a href="/page2"><li>Page 2</li></a>
-        </ul></li>
-        <li><a href="/contact">Contact</a></li>
-      </ul>
+      <NavBar items={[
+        (<span>Home</span>),
+        (<><span>Stuff</span>{NavList({ Page1: '/page1', Page2: '/page2' })}</>),
+        (<span>Contact</span>)
+      ]} />
       {children}
     </StyledApp>
   )
--- app/personal/src/pages/index.jsx
+++ app/personal/src/pages/index.jsx
@@ -1,12 +1,55 @@
+import { useEffect, useRef } from 'react'
+
+
+
+const TheUnconquerable = () => {
+  return (
+    <div class="fader">
+      <h3>The Unconquerable</h3>
+      <p>welcome</p>
+      <p>to the unconquerable pragma</p>
+      <p>this is a test site</p>
+      <p>where we demo things</p>
+      <div style={{ float: 'right', backgroundColor: '#fff' }}><img src="edgewood.png"/></div>
+    </div>
+  )
+}
+
+const TheIndomitable = () => {
+  return (
+    <div class="fader" align="right">
+    <h3>The Indomitable</h3>
+      <p>this is the end of millenia</p>
+      <p>the end of time</p>
+      <p>come to me when you are hollow</p>
+      <p>and i will knit your bones</p>
+      <div class="edgewood" style={{ float: 'left' }}></div>
+    </div>
+  )
+}
+
+
 export default function Home() {
-    return (
-        <div>
-            <h3>The Unconquerable</h3>
-            <p>welcome</p>
-            <p>to the unconquerable pragma</p>
-            <p>this is a test site</p>
-            <p>where we demo things</p>
-        </div>
-    )
+  return (
+    <div>
+      <TheUnconquerable />
+      <TheIndomitable />
+      <TheUnconquerable />
+      <TheIndomitable />
+      <TheUnconquerable />
+      <TheIndomitable />
+      <TheUnconquerable />
+      <TheIndomitable />
+      <TheUnconquerable />
+      <TheIndomitable />
+      <TheUnconquerable />
+      <TheIndomitable />
+      <TheUnconquerable />
+      <TheIndomitable />
+      <TheUnconquerable />
+      <TheIndomitable />
+      
+    </div>
+  )
 
 }
